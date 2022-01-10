1a6c417cc040a683c314e6d27a3974cf4908762b
Justyna Kaczmarek <jkaczmarek@apollogic.com>
2022-01-10 18:03:29 +0100

    dash bootstrap module eliminated

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 
 from pandas.io.formats import style
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
+# import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 import numpy as np
